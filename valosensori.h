64c5b55f6c9931268a30de3b07c4bda21e86013f
kalasa12 <74965617+kalasa12@users.noreply.github.com>
2023-10-20 17:53:38 +0300

    Add files via upload

---
+++ valosensori.h
@@ -0,0 +1,15 @@
+/*
+ * valosensori.h
+ *
+ *  Created on: Oct 17, 2023
+ *      Author: linuxlite
+ */
+
+#ifndef VALOSENSORI_H_
+#define VALOSENSORI_H_
+float valoisuus(uint16_t rekisteri);
+
+
+
+
+#endif /* VALOSENSORI_H_ */
