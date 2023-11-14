4d309816e9f812e0951fc56b875ce2012c675c38
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-11-14 18:28:37 +0200

    sensorit toimii mutta kirjoitus jumittaa

--- project_main.c
+++ project_main.c
@@ -12,6 +12,7 @@
 #include <ti/drivers/PIN.h>
 #include <ti/drivers/pin/PINCC26XX.h>
 #include <ti/drivers/I2C.h>
+#include <ti/drivers/i2c/I2CCC26XX.h>
 #include <ti/drivers/Power.h>
 #include <ti/drivers/power/PowerCC26XX.h>
 #include <ti/drivers/UART.h>
@@ -38,10 +39,11 @@ char messageBuffer[BUFFERSIZE];
 char sensorTaskStack[STACKSIZE];
 char uartTaskStack[STACKSIZE];
 
-double ax, ay, az, gx, gy, gz, temp, press, light;
+float ax, ay, az, gx, gy, gz;
+double temp, press, light;
 int time;
 
-double sensor_data[SENSOR_DATA_ROWS][SENSOR_DATA_COLUMNS];
+float sensor_data[SENSOR_DATA_ROWS][SENSOR_DATA_COLUMNS];
 enum SensorDataKeys { TIME, AX, AY, AZ, GX, GY, GZ, TEMP, PRESS, LIGHT };
 
 // JTKJ: Exercise 3. Definition of the state machine
@@ -53,6 +55,22 @@ static PIN_Handle buttonHandle;
 static PIN_State buttonState;
 static PIN_Handle ledHandle;
 static PIN_State ledState;
+
+// MPU power pin global variables
+static PIN_Handle mpuPinHandle;
+static PIN_State  mpuPinState;
+
+// MPU power pin
+static PIN_Config mpuPinConfig[] = {
+    Board_MPU_POWER  | PIN_GPIO_OUTPUT_EN | PIN_GPIO_HIGH | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+    PIN_TERMINATE
+};
+
+// MPU uses its own I2C interface
+static const I2CCC26XX_I2CPinCfg i2cMPUCfg = {
+    .pinSDA = Board_I2C0_SDA1,
+    .pinSCL = Board_I2C0_SCL1
+};
 
 PIN_Config buttonConfig[] = {
    Board_BUTTON0 | PIN_INPUT_EN | PIN_PULLUP | PIN_IRQ_NEGEDGE,
@@ -125,51 +143,91 @@ void uartTaskFxn(UArg arg0, UArg arg1) {
 
 Void sensorTaskFxn(UArg arg0, UArg arg1) {
 
-   I2C_Handle      i2c;
-   I2C_Params      i2cParams;
+   I2C_Handle      i2c_mpu9250, i2c_opt3001, i2c_bmp280;
+   I2C_Params      i2cParams_mpu9250, i2cParams_opt3001, i2cParams_bmp280;
 
    // Alustetaan i2c-väylä
-   I2C_Params_init(&i2cParams);
-   i2cParams.bitRate = I2C_400kHz;
+   I2C_Params_init(&i2cParams_mpu9250);
+   I2C_Params_init(&i2cParams_opt3001);
+   I2C_Params_init(&i2cParams_bmp280);
+   i2cParams_mpu9250.bitRate = I2C_400kHz;
+   i2cParams_mpu9250.custom = (uintptr_t)&i2cMPUCfg;
+   i2cParams_opt3001.bitRate = I2C_400kHz;
+   i2cParams_bmp280.bitRate = I2C_400kHz;
 
-   // Avataan yhteys
-   i2c = I2C_open(Board_I2C_TMP, &i2cParams);
-   if (i2c == NULL) {
-      System_abort("Error Initializing I2C\n");
-   }
-
-   // JTKJ: Teht�v� 2. Alusta sensorin OPT3001 setup-funktiolla
-   //       Laita enne funktiokutsua eteen 100ms viive (Task_sleep)
-   Task_sleep(100000 / Clock_tickPeriod);
-
-   //TODO: lisää sensors-kansion tiedostoihin puuttuvat rekisteripyörittelyt
-   //TODO: alusta loput sensorit
-   opt3001_setup(&i2c);
-   Task_sleep(100000 / Clock_tickPeriod);
-   mpu9250_setup(&i2c);
-   Task_sleep(100000 / Clock_tickPeriod);
-   bmp280_setup(&i2c);
-
-   short index = 0;
+   int index = 0;
    while (1) {
-      //TODO: lisää loput sensorit
       time = Clock_getTicks();
-      light = opt3001_get_data(&i2c);
-      bmp280_get_data(&i2c, &temp, &press);
-      mpu9250_get_data(&i2c, &ax, &ay, &az, &gx, &gy, &gz);
 
-      write_mpu9250_to_sensor_data(&sensor_data, &index, &ax, &ay, &az, &gx, &gy, &gz);
-      write_other_sensors_to_sensor_data(&sensor_data, &index, &temp, &press, &light);
+      // Avataan MPU9250 yhteys
+      i2c_mpu9250 = I2C_open(Board_I2C_TMP, &i2cParams_mpu9250);
+      if (i2c_mpu9250 == NULL)
+         System_abort("Error Initializing mpu9250 I2C\n");
+      //Käynnistetään MPU9250
+      PIN_setOutputValue(mpuPinHandle,Board_MPU_POWER, Board_MPU_POWER_ON);
+      Task_sleep(100000 / Clock_tickPeriod);
+      mpu9250_setup(&i2c_mpu9250);
+
+      //Haetaan data
+      System_printf("Getting mpu9250 data\n");
+      System_flush();
+      mpu9250_get_data(&i2c_mpu9250, &ax, &ay, &az, &gx, &gy, &gz);
+      //Suljetaan yhteys
+      System_printf("Closing mpu9250 i2c connection\n");
+      System_flush();
+      I2C_close(i2c_mpu9250);
+      //PIN_setOutputValue(mpuPinHandle,Board_MPU_POWER, Board_MPU_POWER_OFF);
+
+      //avataan OPT3001 yhteys
+      i2c_opt3001 = I2C_open(Board_I2C_TMP, &i2cParams_opt3001);
+      if (i2c_opt3001 == NULL)
+         System_abort("Error Initializing opt3001 I2C\n");
+      Task_sleep(100000 / Clock_tickPeriod);
+      opt3001_setup(&i2c_opt3001);
+      
+      //Haetaan data
+      System_printf("Getting opt3001 data\n");
+      System_flush();
+      light = opt3001_get_data(&i2c_opt3001);
+      //suljetaan yhteys
+      System_printf("Closing opt3001 i2c connection\n");
+      System_flush();
+      I2C_close(i2c_opt3001);
+
+      // Avataan BMP280 yhteys
+      i2c_bmp280 = I2C_open(Board_I2C_TMP, &i2cParams_bmp280);
+      if (i2c_bmp280 == NULL)
+         System_abort("Error Initializing mpu9250 I2C\n");
+      Task_sleep(100000 / Clock_tickPeriod);
+      bmp280_setup(&i2c_bmp280);
+      
+      //Haetaan data
+      System_printf("Getting bmp280 data\n");
+      System_flush();
+      bmp280_get_data(&i2c_bmp280, &temp, &press);
+      
+      //Suljetaan yhteys
+      System_printf("Closing bmp280 i2c connection\n");
+      System_flush();
+      I2C_close(i2c_bmp280);
+
+      // Tallennetaan data sensor_data taulukkoon
+      System_printf("Writing sensor data\n");
+      System_flush();
+      write_mpu9250_to_sensor_data(sensor_data, &index, &ax, &ay, &az, &gx, &gy, &gz);
+      write_other_sensors_to_sensor_data(sensor_data, &index, &temp, &press, &light);
 
       if (sensorState == SENSORS_SENDING_DATA) {
-         write_sensor_data_to_messageBuffer(&messageBuffer, &time, &ax, &ay, &az, &gx, &gy, &gz, &temp, &press, &light);
+         write_mpu9250_to_messageBuffer(&messageBuffer, &time, &ax, &ay, &az, &gx, &gy, &gz);
       }
 
       index++;
       if (index == SENSOR_DATA_ROWS)
          index = 0;
-      // Once per second, you can modify this
-      Task_sleep(1000000 / Clock_tickPeriod);
+      System_printf("Sleeping...\n");
+      System_flush();
+      // 10x per second, you can modify this
+      Task_sleep(1000000/10 / Clock_tickPeriod);
    }
 }
 
@@ -184,6 +242,12 @@ int main(void) {
 
    // Initialize board
    Board_initGeneral();
+
+
+   mpuPinHandle = PIN_open(&mpuPinState, mpuPinConfig);
+   if (!mpuPinHandle) {
+      System_abort("Error initializing MPU power pin\n");
+   }
 
    // JTKJ: Exercise 1. Open the button and led pins
    //       Remember to register the above interrupt handler for button
